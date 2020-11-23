9c42e18a0c3c1e74fe4de3890cedc80eac1bc06d
Vojtech Simetka <vojtech@simetka.cz>
2020-11-23 22:05:35 +0100

    feat: topic & network id can be configured

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,3 +1,3 @@
 export const gateway = 'localhost:8080';
-export const topic = 'pss-demo';
-export const beeNetwork = 1;
+export const topic = process.env.BEE_NETWORK_TOPIC || 'pss-demo';
+export const beeNetwork = process.env.BEE_NETWORK_ID || 1;
